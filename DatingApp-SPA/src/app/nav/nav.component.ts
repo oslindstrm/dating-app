2e5684affca4b71308b1fbd3e311be4d79e4b484
DESKTOP-HTT0JVF\Oscar <oscarawet.lindstrom@gmail.com>
2019-10-19 19:35:10 +0100

    routes

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-nav',
@@ -11,16 +12,18 @@ export class NavComponent implements OnInit {
 
   model : any = {};
 
-  constructor(public authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService, private router: Router) { }
 
   ngOnInit() {
   }
 
   login(){
     this.authService.login(this.model).subscribe(next => {
-      this.alertify.success('Logged in successfully');
+      this.alertify.success("Logged in");
     }, error => {
       this.alertify.error(error);
+    }, ()=> {
+      this.router.navigate(["/members"]);
     });
   }
 
@@ -29,8 +32,9 @@ export class NavComponent implements OnInit {
   }
 
   logout(){
-    localStorage.removeItem('token');
+    localStorage.removeItem("token");
     this.alertify.message("Logged out");
+    this.router.navigate(["/home"]);
   }
 
 }
