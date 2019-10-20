c91c1cb36bf04b3eb800bc773beae6f17b556222
DESKTOP-HTT0JVF\Oscar <oscarawet.lindstrom@gmail.com>
2019-10-20 13:59:20 +0100

    Api extensions

--- DatingApp.API/DTOs/UserForDetailedDto.cs
+++ DatingApp.API/DTOs/UserForDetailedDto.cs
@@ -1,15 +1,15 @@
 using System;
 using System.Collections.Generic;
+using DatingApp.API.DTOs;
+using DatingApp.API.Models;
 
-namespace DatingApp.API.Models
+namespace DatingApp.API.Controllers
 {
-    public class User
+    public class UserForDetailedDto
     {
         public int Id { get; set; }
         public string Username { get; set; }
-        public byte[] PasswordHash { get; set; }
-        public byte[] PasswordSalt { get; set; }
-        public DateTime DateOfBirth { get; set; }
+        public int Age { get; set; }
         public DateTime Created { get; set; }
         public DateTime LastActive { get; set; }
         public string Gender { get; set; }
@@ -18,6 +18,7 @@ namespace DatingApp.API.Models
         public string  LookingFor { get; set; }
         public string  Interests { get; set; }
         public string  Country { get; set; }
-        public ICollection<Photo> Photos { get; set; }
+        public string PhotoUrl { get; set; }
+        public ICollection<PhotoForDetailedDto> Photos { get; set; }
     }
 }
